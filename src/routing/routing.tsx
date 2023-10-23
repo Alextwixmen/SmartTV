3ae5f98ba323e6ae9b84b6021da64641e24d0677
Alexander <alexanderlysenko99@mail.ru>
2023-10-24 02:23:14 +0300

    fix/change brouserRouter to hashRouter

--- src/routing/routing.tsx
+++ src/routing/routing.tsx
@@ -1,11 +1,11 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import { VideoPage } from "../Pages/VideoPage/VideoPage";
 import { NumbersPage } from "../Pages/NumbersPage/NumbersPage";
 import { FinalPage } from "../Pages/FinalPage/FinalPage";
 import { Layout } from "../Pages/Layout/Layout";
 import { ErrorPage } from "../Pages/ErrorPage/ErrorPage";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Layout />,
