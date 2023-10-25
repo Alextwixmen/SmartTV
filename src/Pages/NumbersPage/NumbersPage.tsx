44283577a66f69732fed291e10cce9daca7804af
Alexander <alexanderlysenko99@mail.ru>
2023-10-25 21:21:16 +0300

    feature/ add timer

--- src/Pages/NumbersPage/NumbersPage.tsx
+++ src/Pages/NumbersPage/NumbersPage.tsx
@@ -1,13 +1,33 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./NumbersPage.module.css";
 import { NumberEntryZone } from "../../components/NumberEntryZone/NumberEntryZone";
-
+import { useNavigate } from "react-router";
+import { useLocation } from "react-router-dom";
 export type phoneNumbers = number[] | [];
 
 export function NumbersPage() {
+  const navigate = useNavigate();
   const [phoneNumbers, setPhoneNumber] = useState<phoneNumbers>([]);
+  let timer: number;
+
+  useEffect(() => {
+    timer = window.setTimeout(() => {
+      navigate("/video");
+    }, 10000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  // return () => clearInterval(timer);
+
   return (
-    <div className={styles.numbersContiner}>
+    <div
+      className={styles.numbersContiner}
+      onClick={() => clearTimeout(timer)}
+      onKeyDown={() => clearTimeout(timer)}
+    >
       <NumberEntryZone
         setPhoneNumber={setPhoneNumber}
         phoneNumbers={phoneNumbers}
