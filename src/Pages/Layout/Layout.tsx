2b8a764ee7ee28bad29e766680e780ed0f940092
Alexander <alexanderlysenko99@mail.ru>
2023-10-26 00:51:10 +0300

    fix/deleted useless import

--- src/Pages/Layout/Layout.tsx
+++ src/Pages/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import styles from "./Layout.module.css";
 import { Info } from "../../components/Info/Info";
 export function Layout() {
