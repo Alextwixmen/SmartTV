90330e686bcbb1465ee3148ad5b0ebc16f425c6c
Alexander <alexanderlysenko99@mail.ru>
2023-10-25 21:45:22 +0300

    fix/ http

--- src/components/ConfirmButton/ConfirmButton.tsx
+++ src/components/ConfirmButton/ConfirmButton.tsx
@@ -22,7 +22,7 @@ export function ConFirmButton({
   const number = `8${phoneNumbers.join("")}`;
   const handleClick = async () => {
     const response = await fetch(
-      `https://apilayer.net/api/validate?access_key=${
+      `http://apilayer.net/api/validate?access_key=${
         import.meta.env.VITE_ACCESSTOKEN
       }&number=${number}&country_code=${COUNTRY_CODE}&format=${FORMAT}`
     );
